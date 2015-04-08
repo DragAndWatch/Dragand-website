8459e44e21eceb68ff620b641d35250347b39fd3
Patrick HENG <patrick.heng@hetic.net>
2015-04-08 12:47:41 +0200

    Front - Add scroll animation on circles

--- public/assets/js/controllers/mainCtrl.js
+++ public/assets/js/controllers/mainCtrl.js
@@ -1,8 +1,55 @@
 'use strict';
 
 // Click on bottom logo 
-$('#logoLight').on('click', function(){
-	 $('html,body').animate({
-      scrollTop: 0
-    }, 1000);
+$('#logoLight').on('click', function() {
+    UI.scrollToTop(1000);
 });
+
+
+$(window).scroll(function() {
+	checkCircleAnimation();
+});
+$(window).on('resize', function() {
+	checkCircleAnimation();
+});
+
+
+// Trigger circle animation on scroll
+function checkCircleAnimation() {
+    $('.circle').each(function(i) {
+        var bottom_of_object = $(this).offset().top;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if (bottom_of_window > bottom_of_object) {
+            $(this).addClass('visible');
+        }
+    });
+}
+
+
+
+// Add Twitter button script
+window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    t._e = [];
+    t.ready = function(f) {
+        t._e.push(f);
+    };
+    return t;
+}(document, "script", "twitter-wjs"));
+
+// Add Facebook Btn 
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=732296003534824&version=v2.3";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
--- public/assets/js/views/UI.js
+++ public/assets/js/views/UI.js
@@ -1,25 +1,24 @@
-// Add Twitter button script
-window.twttr = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    t._e = [];
-    t.ready = function(f) {
-        t._e.push(f);
-    };
-    return t;
-}(document, "script", "twitter-wjs"));
+'use strict';
 
-// Add Facebook Btn 
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=732296003534824&version=v2.3";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+var UI = {
+    scrollToTop : function(duration){
+        $('html,body').animate({
+          scrollTop: 0
+        }, duration);
+    };
+
+
+}
+
+
+
+
+
+$('.slideLeft').each(function (i) {
+                var bottom_of_object = $(this).offset().top;
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+                if (bottom_of_window > bottom_of_object) {
+                    $(this).addClass('animated fadeInLeft');
+                 }
+             });
