42eef7e965e330f701223f27ea96ea09d2e51c06
Patrick HENG <patrick.heng@hetic.net>
2015-04-08 23:00:00 +0200

    Front - Integration of API stats without animation

--- public/assets/js/views/UI.js
+++ public/assets/js/views/UI.js
@@ -6,19 +6,4 @@ var UI = {
           scrollTop: 0
         }, duration);
     };
-
-
 }
-
-
-
-
-
-$('.slideLeft').each(function (i) {
-                var bottom_of_object = $(this).offset().top;
-                var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-                if (bottom_of_window > bottom_of_object) {
-                    $(this).addClass('animated fadeInLeft');
-                 }
-             });
