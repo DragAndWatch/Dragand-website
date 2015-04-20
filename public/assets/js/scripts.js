974fbb8c2e0288947844a38b7c4fc7372f7d9abf
Patrick HENG <patrick.heng@hetic.net>
2015-04-21 00:12:14 +0200

    Front - Add twitter follow button

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -88,7 +88,7 @@ $(document).ready(function() {
     });
 
 
-    // ---- Add Twitter button script
+    // ---- Add Twitter share script
     window.twttr = (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0],
             t = window.twttr || {};
@@ -103,5 +103,8 @@ $(document).ready(function() {
         };
         return t;
     }(document, "script", "twitter-wjs"));
+
+    // ---- Add Twitter follow script
+   window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));
 
 });
